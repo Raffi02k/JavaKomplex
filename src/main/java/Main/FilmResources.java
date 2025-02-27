bdf845b00de1013f247f089d00ee5e1489ba2a76
Raffi <raffi9998@gmail.com>
2025-02-27 11:24:55 +0100

    Made a Film class.

--- src/main/java/Main/FilmResources.java
+++ src/main/java/Main/FilmResources.java
@@ -1,7 +1,16 @@
 package Main;
 
+import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
 
 @Path("films")
 public class FilmResources {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Film firstTest() {
+        return new Film("title", 2002);
+    }
 }
