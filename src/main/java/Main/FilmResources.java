d8e2a8e1678e7a22b01749df635024c1cae37058
Raffi <raffi9998@gmail.com>
2025-03-04 13:53:00 +0100

    implemented in FilmResources @Post and @Put Methods.

--- src/main/java/Main/FilmResources.java
+++ src/main/java/Main/FilmResources.java
@@ -5,11 +5,9 @@ import exaption.NotFound;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.PathParam;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
 import lombok.extern.java.Log;
 import repository.FilmRepositoory;
 
@@ -58,10 +56,38 @@ public class FilmResources {
     @GET
     @Path("{id}")
     @Produces(MediaType.APPLICATION_JSON)
-    public Film getOneMovie(@PathParam("id") Long id){
+    public Film getOneMovie(@PathParam("id") Long id) {
         return filmRepositoory.findById(id).orElseThrow(
-                () -> new NotFound("Movie with ID: " + id +" not found")
+                () -> new NotFound("Movie with ID: " + id + " not found")
         );
     }
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Film> getMovies() {
+        return filmRepositoory.findAll().toList();
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response createNewFilm(Film film) {
+        var newFilm = filmRepositoory.save(film);
+        return Response
+                .status(Response.Status.CREATED)
+                .header("Location", "/api/films" + newFilm.getId())
+                .build();
+    }
+
+    @PUT
+    @Path("{id}")
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response updateFilm(Film film, @PathParam("id") Long id) {
+        filmRepositoory.findById(id).orElseThrow(() -> new NotFound("Movie with ID: " + id + " not found"));
+        film.setId(id);
+        filmRepositoory.save(film);
+        return Response.noContent().build();
+
+    }
+
 }
 
