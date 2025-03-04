05112e3d91dc39b72e577b1e19261242c80271b3
Raffi <raffi9998@gmail.com>
2025-03-04 09:28:21 +0100

    updated Entity class with more information holders.

---
+++ src/main/java/dto/FilmDTO.java
@@ -0,0 +1,4 @@
+package dto;
+
+public record FilmDTO (int year, String title, String genre, String director, String actors) {
+}
--- src/main/java/entity/Film.java
+++ src/main/java/entity/Film.java
@@ -1,7 +1,11 @@
 package entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Past;
 
+import java.time.LocalDate;
 import java.util.Objects;
 
 @Entity
@@ -9,47 +13,45 @@ public class Film {
     public Film() {
     }
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
-    public Long id;
+    @NotBlank(message = "Title is required")
     private String title;
+
+    @Min(value = 1888, message = "Year must be after the first film was created")
     private int year;
+
+    @NotBlank(message = "Director is required")
+    private String director;
+
+    @Min(value = 1, message = "Duration must be greater than 0")
+    private int duration;
+
+    @Past LocalDate releaseDate;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long getId() {
-        return id;
-    }
+    @Column(nullable = false)
+    private Long id;
 
     public Film(String title, int year) {
         this.title = title;
         this.year = year;
     }
 
-
-    @Override
-    public String toString() {
-        return "Film{" +
-                "title='" + title + '\'' +
-                '}';
+    public Film(String title, int year, String director, int duration, LocalDate releaseDate) {
+        this.title = title;
+        this.year = year;
+        this.director = director;
+        this.duration = duration;
+        this.releaseDate = releaseDate;
     }
 
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Film film = (Film) o;
-        return id != null && Objects.equals(id, film.id);
+
+    public Long getId() {
+        return id;
     }
 
     public void setId(Long id) {
         this.id = id;
-    }
-
-    @Override
-    public final int hashCode() {
-        return getClass().hashCode();
     }
 
     public String getTitle() {
@@ -66,5 +68,53 @@ public class Film {
 
     public void setYear(int year) {
         this.year = year;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    @Override
+    public String toString() {
+        return "Film{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", year=" + year +
+                ", director='" + director + '\'' +
+                ", duration=" + duration +
+                '}';
+    }
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Film film = (Film) o;
+        return id != null && Objects.equals(id, film.id);
+    }
+
+    @Override
+    public final int hashCode() {
+        return getClass().hashCode();
     }
 }
